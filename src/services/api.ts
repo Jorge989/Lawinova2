06945a5fc8b88c8fe738fe98457ea0233eb31b8f
Jorge989 <attiejorge89@gmail.com>
2021-02-15 12:24:50 -0300

    committeste

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 let api = axios.create({
-    baseURL:'https://inova-actionsys.herokuapp.com/',
+    baseURL:'https://inova-backend-dev.azurewebsites.net/',
   headers:{
     'Access-Control-Allow-Origin': '*' 
   }
